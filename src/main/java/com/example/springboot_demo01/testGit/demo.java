d265183aa814cf1d7e9ae42e6f5a31627eb0f052
blad <win@qq.com>
2023-01-25 17:32:08 +0800

    my commit

---
+++ src/main/java/com/example/springboot_demo01/testGit/demo.java
@@ -0,0 +1,8 @@
+package com.example.springboot_demo01.testGit;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello first");
+        System.out.println("hello second");
+    }
+}
